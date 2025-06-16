29f7f9efb9ec5f5b7bf9ad0cafd0254130566ceb
mcbosch <mciudadbosch@gmail.com>
2025-06-16 20:16:12 +0200

    matrix distance kernels

--- Weisfeirel-Lehman/WeisfeirelLehman_m.py
+++ Weisfeirel-Lehman/WeisfeirelLehman_m.py
@@ -1,3 +1,7 @@
+import grakel as gl
+import numpy as np
+import sys
+
 def WeisferLheman(G, n_iter):
     r"""
     G is a list of graphs wich each graph is saved as:
@@ -17,13 +21,12 @@ def WeisferLheman(G, n_iter):
     
     L = list(first_labels)
     idx_labels += len(L)
-
     # A cada node li assignam lo que sirà la seva representació
     labels_count = {k: {i: 0 for i in range(len(L))} for k in range(len(G))}
     WL_inverse_labels = {L[i]: i for i in range(len(L))}
 
     for k in range(len(G)):
-        (g, l) = G[k]
+        g, l = G[k][0], G[k][1]
         for v in g.keys(): 
             # Assignam nou label a cada node
             l[v] = WL_inverse_labels[l[v]]
@@ -34,7 +37,7 @@ def WeisferLheman(G, n_iter):
     for i in range(n_iter):
         new_labels = set()
         for k in range(len(G)):
-            (g,l) = G[k]
+            g,l = G[k][0], G[k][1]
             actual_node_labels = {}
             # Feim reassignació
             for v in g.keys():
@@ -59,20 +62,21 @@ def WeisferLheman(G, n_iter):
 
         # Canviam els labels per l'assignació que hem fet i contam labels
         for k in range(len(G)):
-            (g,l)= G[k]
+            g, l= G[k][0], G[k][1]
             for v in g.keys(): 
                 # Assignam nou label a cada node
                 l[v] = WL_inverse_labels[l[v]]
                 # Sumam 1 a cada label
                 labels_count[k][l[v]]+= 1
+                
 
     info = '\nINFORMATION OF WEISFEIR-LEHMAN REPRESENTATION:\n' + \
                 f'  Yo have a total of {len(G)} graphs, each graph\n' + \
                 f'  has an Euclidian representation of legth {len(labels_count[0])}.'
-    print(info)
+    #print(info)
     return labels_count
 
-def graph_labeled(n,S,D,labels = dict()):
+def graph_labeled(n,S,D,labels = dict(),grak = False):
     r"""
     This function recieves a set of edges, S (source) and D
     (destination) of a graph with n noodes. The dictionary 
@@ -89,4 +93,29 @@ def graph_labeled(n,S,D,labels = dict()):
     for i in range(len(S)):
         graph[D[i]].append(S[i])
 
-    return (graph, labels) 
+    return (graph, labels) if not grak else gl.Graph(graph, labels)
+
+
+def matrix_dist(graphs, n_iter):
+    n = len(graphs)
+    distance_matrix = np.zeros((n,n))
+    for i in range(n):
+        for j in range(i,n):
+            if i == j:
+                label_counts = WeisferLheman([graphs[i]], n_iter=n_iter)
+                r1,r2 = np.array(list(label_counts[0].values())), np.array(list(label_counts[0].values()))
+            else:
+                label_counts = WeisferLheman([graphs[i], graphs[j]], n_iter=n_iter)
+                r1, r2 = np.array(list(label_counts[0].values())), np.array(list(label_counts[1].values()))
+            distance_matrix[i,j], distance_matrix[j,i] = np.dot(r1,r2), np.dot(r1,r2)
+            # Print formato carga
+        char = f"\033[7m \033[0m"*int(100*i/(n)) + " "*(100-int(100*i/(n)))
+        sys.stdout.write(f"\r|{char}|")  # Escribe en la misma línea
+        sys.stdout.flush()  # Forzar la actualización de la línea
+
+    f = " "*102 
+    sys.stdout.write(f"\r{f}")
+    sys.stdout.write(f"\rDistance matrix computed!\n")
+    sys.stdout.flush()
+
+    return distance_matrix
